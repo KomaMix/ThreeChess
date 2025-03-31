cbb4d8e762d39ed69be27404e4a0ff782de07628
KomaMix <ksabirov448@gmail.com>
2025-03-31 20:15:45 +0300

    added database

---
+++ ThreeChess/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ThreeChess.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+}
--- ThreeChess/Program.cs
+++ ThreeChess/Program.cs
@@ -1,10 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ThreeChess.Data;
 using ThreeChess.Hubs;
 using ThreeChess.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.WebHost.UseUrls(
+    $"http://0.0.0.0:{7288}"
+);
+
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
 
 builder.Services.AddTransient<BoardCreateService>();
 
@@ -17,6 +37,10 @@ app.UseStaticFiles();
 app.MapHub<MoveHub>("/move");
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 
