01df55ad91829e5bb16be85d388d61ffc7881018
KomaMix <ksabirov448@gmail.com>
2025-06-08 20:21:40 +0300

    added highlight start cell

--- ThreeChess/wwwroot/scripts/highlightMoves.js
+++ ThreeChess/wwwroot/scripts/highlightMoves.js
@@ -1,12 +1,24 @@
 ﻿function highlightMoves(cell) {
     const piece = boardElementsState.cells[cell.id].elements.figure;
 
+    highlightStartCell(cell.id);
+
     cellsIds = getPossibleMoves(cell.id);
     highlightCellsArray(cellsIds)
 }
 
 function highlightCellsArray(cellIds) {
     cellIds.forEach(cellId => highlightCell(cellId))
+}
+
+function highlightStartCell(cellId) {
+    const cell = boardElementsState.cells[cellId];
+    if (!cell) {
+        console.error(`нет клетки с id ${cellId}`)
+    }
+    if (cell.elements.figure) {
+        cell.elements.path.classList.add('cell-selected');
+    }
 }
 
 function highlightCell(targetCellId) {
@@ -25,5 +37,6 @@ function clearHighlightedCells() {
     document.querySelectorAll('path').forEach(path => {
         path.classList.remove('cell-highlighted');
         path.classList.remove('cell-capture-highlight');
+        path.classList.remove('cell-selected');
     });
 }
